23c6dd3f0eb93c79e72595bce9295cec2349c793
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-03-11 21:20:47 +0300

    Добавьте файлы проекта.

---
+++ Antizapret/Program.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Antizapret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://api.antizapret.info");
+            var request = new RestRequest("group.php");
+            request.AddParameter("data", "domain");
+            request.AddParameter("type", "json");
+
+            var response = client.Get(request);
+            var content = response.Content;
+            var domains = content.Replace("\n", "\nadd list=blocklist address=");
+
+            string writePath = @"file.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(domains);
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
