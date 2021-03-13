2d1c0ebf8bbfe41a37a01a1081b4caca90557697
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-03-13 19:50:54 +0300

    1

--- Antizapret/Program.cs
+++ Antizapret/Program.cs
@@ -18,47 +18,47 @@ namespace Antizapret
 
             var response = client.Get(request);
 
-            var domains = response.Content.Replace("*.", "").Split("\n").ToList();
+            var domains = response.Content.Replace("*.", "").Split("\n").Select(e => e.Split(".")).ToList();
+            var resultList = new List<string>();
 
             for (int i = 0; i < domains.Count; i++)
             {
-                if(Regex.IsMatch(domains[i], @"\p{IsCyrillic}"))
+                var array = domains[i];
+
+                foreach (var s in domains[i])
                 {
-                    domains[i] = "";
+                    if (Regex.IsMatch(s, @"\p{IsCyrillic}"))
+                    {
+                        domains[i] = null;
+                        continue;
+                    }
+                }
+
+                var str = new string[2];
+                if (array.Length <= 1)
+                {
                     continue;
                 }
-                var domain = domains[i].Split(".");
-                var str = new string [2];
-                if(domain.Length <= 1)
+                if (array.Count() > 2)
                 {
-                    continue;
-                }
-                if (domain.Count() > 2)
-                {
-                    str[0] = domain[domain.Length - 2];
-                    str[1] = domain[domain.Length - 1];
+                    str[0] = array[array.Length - 2];
+                    str[1] = array[array.Length - 1];
                 }
                 else
                 {
-                    str[0] = domain[0];
-                    str[1] = domain[1];
+                    str[0] = array[0];
+                    str[1] = array[1];
                 }
 
                 if (str[1] == "ru" || str[1] == "com" || str[1] == "org" || str[1] == "net")
                 {
-                    domains[i] = $"add list=blocklist address={str[0]}.{str[1]}";
-                }
-                else
-                {
-                    domains[i] = "";
+                    resultList.Add($"add list=blocklist address={str[0]}.{str[1]}");
                 }
             }
 
-            domains.RemoveAll(e => e == "");
+            resultList = resultList.Distinct().ToList();
 
-            domains = domains.Distinct().ToList();
-
-            string result = string.Join("\n", domains);
+            string result = string.Join("\n", resultList);
 
             string writePath = @"file.txt";
 
