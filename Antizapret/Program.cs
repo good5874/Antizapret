1b07d98b793bddbac1a206083b3ccc11aec3efde
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-03-13 09:55:06 +0300

    12

--- Antizapret/Program.cs
+++ Antizapret/Program.cs
@@ -19,22 +19,38 @@ namespace Antizapret
             var response = client.Get(request);
 
 
-            var domains = response.Content.Replace("*.","").Split("\n").ToList();
+            var domains = response.Content.Replace("*.", "").Split("\n").ToList();
 
             for (int i = 0; i < domains.Count; i++)
             {
                 var domain = domains[i].Split(".");
-                if (domain.Last() == "ru" || domain.Last() == "com" || domain.Last() == "org" || domain.Last() == "net")
+                var str = new string [2];
+                if(domain.Length <= 1)
                 {
-                    domains[i] = $"add list=blocklist address={domains[i]}";
+                    continue;
+                }
+                if (domain.Count() > 2)
+                {
+                    str[0] = domain[domain.Length - 2];
+                    str[1] = domain[domain.Length - 1];
                 }
                 else
                 {
-                    domains[i]="";
+                    str[0] = domain[0];
+                    str[1] = domain[1];
+                }
+
+                if (str[1] == "ru" || str[1] == "com" || str[1] == "org" || str[1] == "net")
+                {
+                    domains[i] = $"add list=blocklist address={str[0]}.{str[1]}";
+                }
+                else
+                {
+                    domains[i] = "";
                 }
             }
 
-            domains.RemoveAll(e=>e=="");
+            domains.RemoveAll(e => e == "");
 
             domains = domains.Distinct().ToList();
 
